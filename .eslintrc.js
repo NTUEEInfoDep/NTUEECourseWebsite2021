eb5278ccfec10b09fc58acfb9a757eedd754d575
Kenchu123 <k88913n@gmail.com>
2021-03-12 20:44:46 +0800

    environment

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js", ".jsx"],
+      },
+    ],
+    "react/jsx-props-no-spreading": "off",
+  },
+  overrides: [
+    {
+      files: ["server/**"],
+      rules: {
+        "no-console": "off",
+        "no-unused-vars": "off",
+      },
+    },
+  ],
+};
