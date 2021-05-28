01f52ce3a22ee17d36a00c154c0ce8743340d9cb
caffrey928 <caffreyacnologialawace@gmail.com>
2021-05-28 19:17:31 +0800

    login add handlesubmit

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,52 +1,49 @@
-import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-// import Link from '@material-ui/core/Link';
-// import Grid from '@material-ui/core/Grid';
-import Box from "@material-ui/core/Box";
-import LockTwoTone from "@material-ui/icons/LockTwoTone";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import IconButton from "@material-ui/core/IconButton";
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
-import Autocomplete from "@material-ui/lab/Autocomplete";
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
+import LockTwoTone from '@material-ui/icons/LockTwoTone';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      NTUEE {new Date().getFullYear()}
-      {"."}
+      {'Copyright © '}
+      NTUEE
+      {' '}
+      {new Date().getFullYear()}
+      {'.'}
     </Typography>
   );
 }
 
-const options = ["English", "中文"];
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(25),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    border: "gray 1px solid",
-    borderRadius: "3%",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    border: 'gray 1px solid',
+    borderRadius:'3%',
   },
   avatar: {
     margin: theme.spacing(1),
     marginTop: theme.spacing(3),
     backgroundColor: theme.palette.success.light,
-    height: "70px",
-    width: "70px",
+    height:'70px',
+    width:'70px',
   },
   form: {
-    width: "80%",
+    width: '80%',
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(3),
   },
@@ -56,76 +53,79 @@ const useStyles = makeStyles((theme) => ({
   accountmargin: {
     marginBottom: theme.spacing(2),
   },
-  language: {
-    marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(127),
-  },
 }));
 
 export default function Login() {
   const classes = useStyles();
 
   const [values, setValues] = React.useState({
-    account: "",
-    password: "",
+    account:'',
+    password:'',
     showPassword: false,
-    language: options[0],
+    error: false,
   });
-
+  
   const handleChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (/^(b|r|d)\d{8}$/i.test(values.account) && !/^$/i.test(values.password)) {
+      values.error = false;
+      setValues({ ...values, account: '',password:'',showPassword:'' })
+    } else {
+      values.error = true;
+      setValues({ ...values, account: '',password:'',showPassword:'' })
+    }
   };
 
   const handleClickShowPassword = () => {
     setValues({ ...values, showPassword: !values.showPassword });
   };
-
+  
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
 
   return (
     <div>
-      <Container component="main" maxWidth="xs">
+      <Container component="main" maxWidth='xs'>
         <CssBaseline />
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockTwoTone style={{ fontSize: 40 }} />
           </Avatar>
           <Typography component="h1" variant="h5">
-            {values.language === options[0] ? (
-              <div>Sign in</div>
-            ) : (
-              <div>登入</div>
-            )}
+            <div>Sign in</div>
           </Typography>
-          <form className={classes.form} noValidate>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
             <OutlinedInput
+              error={values.error}
               margin="normal"
               required
               fullWidth
               id="StudentId"
-              placeholder={
-                values.language === options[0] ? "Student ID" : "學號"
-              }
+              placeholder="Student ID"
               value={values.account}
               name="StudentId"
               className={classes.accountmargin}
               autoComplete="StudentId"
-              onChange={handleChange("account")}
+              onChange={handleChange('account')}
               autoFocus
             />
             <OutlinedInput
+              error = {values.error}
               margin="normal"
               required
               fullWidth
               name="password"
-              placeholder={values.language === options[0] ? "Password" : "密碼"}
+              placeholder="Password"
               value={values.password}
               id="password"
-              type={values.showPassword ? "text" : "password"}
+              type={values.showPassword ? 'text' : 'password'}
               autoComplete="current-password"
-              onChange={handleChange("password")}
+              onChange={handleChange('password')}
               endAdornment={
                 <InputAdornment position="end">
                   <IconButton
@@ -134,11 +134,12 @@ export default function Login() {
                     onMouseDown={handleMouseDownPassword}
                     edge="end"
                   >
-                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
                   </IconButton>
                 </InputAdornment>
               }
             />
+            <div style={{color:'red'}}>{values.error? "Incorrect account or password":null}</div>
             <Button
               type="submit"
               fullWidth
@@ -146,11 +147,7 @@ export default function Login() {
               color="primary"
               className={classes.submit}
             >
-              {values.language === options[0] ? (
-                <div>Sign in</div>
-              ) : (
-                <div>登入</div>
-              )}
+              <div>Sign in</div>
             </Button>
             <Box>
               <Copyright />
@@ -158,17 +155,6 @@ export default function Login() {
           </form>
         </div>
       </Container>
-      <Autocomplete
-        value={values.language}
-        onChange={(event, newValue) => {
-          setValues({ ...values, language: newValue });
-        }}
-        className={classes.language}
-        id="language"
-        options={options}
-        style={{ width: 140 }}
-        renderInput={(params) => <TextField {...params} variant="outlined" />}
-      />
     </div>
   );
 }
