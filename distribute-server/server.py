87e6917be7142a615bc1df4557207e63d594302e
MortalHappiness <b07901069@ntu.edu.tw>
2021-06-04 22:12:12 +0800

    Add distribute server

---
+++ distribute-server/server.py
@@ -0,0 +1,29 @@
+import os
+import uuid
+from flask import Flask
+
+# ========================================
+
+app = Flask(__name__)
+
+PORT = 8001
+if "PORT" in os.environ:
+    PORT = os.environ["PORT"]
+
+# ========================================
+
+
+@app.route("/")
+def index():
+    return "distribute server"
+
+
+@app.route("/distribute", methods=["POST"])
+def distribute():
+    return f"Hello, hi"
+
+# ========================================
+
+
+if __name__ == "__main__":
+    app.run(port=PORT)
