4c174fd1d4c8d48e79f1debd8c0da3ef8705393c
Michael Lai <michaellai901026@gmail.com>
2021-06-13 18:59:26 +0800

    finish algorithm (except preselect)

---
+++ distribute-server/test.py
@@ -0,0 +1,18 @@
+from algorithm import Algorithm, Course, Student
+
+
+student1 = Student("a", "B00000000",
+        {"course1":["teacher1a","teacher1b"]}, 2)
+student2 = Student("b", "B11111111", {"course1":["teacher1a","teacher1b"]}, 1)
+student3 = Student("c", "B22222222",
+        {"course1":["teacher1a","teacher1b"]}, 8)
+students = [student2,student1,student3]
+
+course1 = Course({"id":"course1", "name": "course1", "type": "EE-Lab",
+    "options":{"teacher1a": 1, "teacher1b": 1}})
+courses = [course1]
+
+print(Algorithm.distribute(courses, students))
+
+
+
--- distribute-server/test_algorithm.py
+++ distribute-server/test_algorithm.py
@@ -9,6 +9,21 @@ class Test(unittest.TestCase):
         results = []
         self.assertEqual(Algorithm.distribute(courses, students), results)
 
+    def test_02_empty(self):
+
+        student1 = Student("Michael", "B09901186",
+        {"course1":["teacher1a","tescher1b"]}, 2)
+        student2 = Student("Mecoli", "610736",
+        {"course1":["teacher1a","tescher1b"]}, 1)
+        students = [student1,student2]
+
+
+        courses = []
+
+        results = []
+
+        self.assertEqual(Algorithm.distribute(courses, students), results)
+
 
 if __name__ == "__main__":
     unittest.main()
