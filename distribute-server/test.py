36aa59053b55a1429898aee2e9a563ad757b6793
Michael Lai <michaellai901026@gmail.com>
2021-06-18 09:16:56 +0800

    complete ten-select-two

--- distribute-server/test.py
+++ distribute-server/test.py
@@ -5,11 +5,11 @@ student1 = Student("a", "B00000000",
         {"course1":["teacher1a","teacher1b"]}, 2)
 student2 = Student("b", "B11111111", {"course1":["teacher1a","teacher1b"]}, 1)
 student3 = Student("c", "B22222222",
-        {"course1":["teacher1a","teacher1b"]}, 8)
+        {"course1":["teacher1b","teacher1a"]}, 8)
 students = [student2,student1,student3]
 
-course1 = Course({"id":"course1", "name": "course1", "type": "EE-Lab",
-    "options":{"teacher1a": 1, "teacher1b": 1}})
+course1 = Course({"id":"course1", "name": "course1", "type": "Ten-Select-Two",
+    "options":{"teacher1a": 2, "teacher1b": 2}})
 courses = [course1]
 
 print(Algorithm.distribute(courses, students))
