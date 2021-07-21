bb15b74c049dd97df65ecef2e8ee4791e25f09e0
monica0325 <b09901046@ntu.edu.tw>
2021-07-21 16:47:59 +0800

    selection_course

--- src/containers/selection/course.js
+++ src/containers/selection/course.js
@@ -1,32 +1,51 @@
-import { Container } from "@material-ui/core";
+// import { Container } from "@material-ui/core";
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  styledCourse: {
+    backgroundColor: "#eee",
+    borderRadius: 4,
+    padding: "4px 8px",
+    transition: "background-color .8s ease-out",
+    marginTop: 8,
+    color: "#071214",
+
+    ":hover": {
+      backgroundColor: "#fff",
+      transition: "background-color .1s ease-in",
+    },
+  },
+});
 const Course = ({ courseId, course, index }) => {
+  const classes = useStyles();
   return (
     <Draggable draggableId={courseId} index={index}>
       {(provided) => (
-        <Container
+        <div
+          className={classes.styledCourse}
+          ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          innerRef={provided.innerRef}
         >
+          {index + 1}
           {course}
-        </Container>
+        </div>
       )}
-      ;
     </Draggable>
   );
 };
 
 Course.propTypes = {
   courseId: PropTypes.string.isRequired,
-  course: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    content: PropTypes.string.isRequired,
-  }).isRequired,
-  index: PropTypes.string.isRequired,
+  course: PropTypes.string.isRequired,
+  // course: PropTypes.shape({
+  //   id: PropTypes.string.isRequired,
+  //   content: PropTypes.string.isRequired,
+  // }).isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default Course;
