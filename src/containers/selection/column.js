e0e29651bfe2d3010a222c6c796c784c0a192812
monica0325 <b09901046@ntu.edu.tw>
2021-07-21 16:49:46 +0800

    selection_column

--- src/containers/selection/column.js
+++ src/containers/selection/column.js
@@ -1,43 +1,76 @@
 import React from "react";
-import { Container, List } from "@material-ui/core";
+import { List } from "@material-ui/core";
 import { Droppable } from "react-beautiful-dnd";
 import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
 import Course from "./course";
 
-const Column = ({ columnId, column, options }) => {
+const useStyles = makeStyles({
+  styledColumn: {
+    padding: "24px 0",
+    display: "flex",
+    flexDirection: "column",
+    marginTop: 8,
+    textAlign: "center",
+    h2: {
+      margin: 0,
+      padding: "0 16px",
+    },
+  },
+  styledList: {
+    backgroundColor: "#ddd",
+    borderRadius: 8,
+    padding: 16,
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+    marginTop: 8,
+    textAlign: "center",
+  },
+});
+const Column = ({ columnId, title, column }) => {
+  const classes = useStyles();
   return (
-    <Container>
-      <h2>{column[columnId].title}</h2>
-      <Droppable droppableId={column.id}>
-        {(provided) => (
-          <List innerRef={provided.innerRef} {...provided.droppableProps}>
-            {/* {courses.map((course) => (
-              <Course key={course.id} course={course} index={columnId} />
-            ))} */}
-            {options.forEach((element) => {
-              <Course key={element} course={element} index={columnId} />;
-            })}
+    <Droppable droppableId={columnId}>
+      {(provided) => (
+        <div className={classes.styledColumn}>
+          <h2>{title}</h2>
+          <List
+            className={classes.styledList}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {column
+              ? column.map((element, index) => (
+                  <Course key={element} course={element} index={index} />
+                ))
+              : null}
             {provided.placeholder}
           </List>
-        )}
-      </Droppable>
-    </Container>
+        </div>
+      )}
+    </Droppable>
   );
 };
 
 Column.propTypes = {
-  column: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    courseId: PropTypes.arrayOf(PropTypes.string),
-  }).isRequired,
-  columnId: PropTypes.number.isRequired,
-  options: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired,
-    })
-  ).isRequired,
+  column: PropTypes.arrayOf(PropTypes.string).isRequired,
+  columnId: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default Column;
+// Column.propTypes = {
+//   column: PropTypes.shape({
+//     id: PropTypes.string.isRequired,
+//     title: PropTypes.string.isRequired,
+//     optionIds: PropTypes.arrayOf(PropTypes.string),
+//   }).isRequired,
+//   columnId: PropTypes.string.isRequired,
+//   // options: PropTypes.arrayOf(
+//   //   PropTypes.shape({
+//   //     id: PropTypes.string.isRequired,
+//   //     content: PropTypes.string.isRequired,
+//   //   })
+//   // ).isRequired,
+// };
