860f356525b8312e6370ed2c84754e14d15ca73f
itzsyboo <55495526+Fiona1121@users.noreply.github.com>
2021-07-27 11:47:13 +0800

    add style

--- src/theme.js
+++ src/theme.js
@@ -1,26 +1,44 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Ubuntu Condensed", "sans-serif"].join(","),
+    fontSize: 15,
+  },
   palette: {
     type: "dark",
     primary: {
-      main: "#1a237e",
-      contrastText: "#fff",
+      main: "#b2ebf2",
+      contrastText: "#000",
     },
     secondary: {
-      main: "#ffb74d",
-      light: "ffac33",
-      contrastText: "#fff",
+      main: "#b2dfdb",
+      light: "#c1e5e2",
+      contrastText: "#000",
+    },
+    background: {
+      paper: "rgba(0, 0, 0, 0.7)",
     },
   },
   breakpoints: {
     values: {
       xs: 0,
       sm: 600,
-      phone:700,
+      phone: 700,
       md: 960,
       lg: 1280,
       xl: 1920,
+    },
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        body: {
+          background: "linear-gradient(170deg, #192231 55%, #B6A19E 10%)",
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+        },
+      },
     },
   },
 });
