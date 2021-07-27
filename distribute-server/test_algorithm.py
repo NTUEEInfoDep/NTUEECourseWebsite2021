d41bdc33695e90d2233cdede6416842b407cbb3b
connor226 <connor.taipei@gmail.com>
2021-07-28 04:32:03 +0800

    add 7 tests

--- distribute-server/test_algorithm.py
+++ distribute-server/test_algorithm.py
@@ -15,7 +15,7 @@ class Test(unittest.TestCase):
         results = []
         self.assertEqual(Algorithm.distribute(courses, students), results)
 
-    def test_02_required(self):  # 必修課高年級優先低年級
+    def test_02_required(self):  # test for priority = -1
         student1 = Student(
             "Michael",
             "B09901186",
@@ -33,7 +33,7 @@ class Test(unittest.TestCase):
         course1 = Course({
             "id": "course1",
             "name": "電路學",
-            "type": "Required",
+            "type": "1",
             "description": "",
             "options": {
                 "teacher1a": {"limit": 1, "priority": -1},
@@ -61,8 +61,7 @@ class Test(unittest.TestCase):
 
         self.assertEqual(results, expected)
 
-    # @unittest.skip("maintaining")
-    def test_03(self):  # 測隨機
+    def test_03_priority_0(self):  # test for priority = 0
         student1 = Student("Michael", "B09901186",
                            {"Electronic-Circuits-Experiment": ["teacher1a", "teacher1b"]}, 2)
         student2 = Student("Mecoli", "610736",
@@ -87,8 +86,36 @@ class Test(unittest.TestCase):
 
         self.assertEqual(len(results), 2)
 
-    def test_04_priority_4_3(self):  # 十選二有三年級優先時
+    def test_04_priority_2(self):  # test for priority = 2
+        student1 = Student("Michael", "B09901186",
+                           {"Ten-Select-Two": ["自動控制"]}, 2)
+        student2 = Student("Mecoli", "610736",
+                           {"Ten-Select-Two": ["自動控制"]}, 4)
+        students = [student1, student2]
 
+        course1 = Course({
+            "id": "Ten-Select-Two",
+            "name": "十選二實驗",
+            "type": "Ten-Select-Two",
+            "options": {
+                "自動控制": {"limit": 1, "priority": 2},
+            }
+        })
+        courses = [course1]
+
+        expected = [{
+            "studentID": "B09901186",
+            "courseName": "十選二實驗",
+            "optionName": "自動控制",
+        }
+        ]
+
+        results = list_to_set(Algorithm.distribute(courses, students))
+        expected = list_to_set(expected)
+
+        self.assertEqual(results, expected)
+
+    def test_05_priority_3(self):  # test for priority = 3
         student1 = Student("Michael", "B09901186",
                            {"Ten-Select-Two": ["自動控制", "電力電子"]}, 3)
         student2 = Student("Mecoli", "610736",
@@ -123,8 +150,105 @@ class Test(unittest.TestCase):
 
         self.assertEqual(results, expected)
 
-    def test_05_ten_select_two(self):  # 測多測資
+    def test_06_priority_4(self): # test for priority = 4
+        student1 = Student("Michael", "B09901186",
+                           {"Ten-Select-Two": ["自動控制", "電力電子"]}, 4)
+        student2 = Student("Mecoli", "610736",
+                           {"Ten-Select-Two": ["自動控制", "電力電子"]}, 3)
+        student3 = Student("Michael Jackson", "ABC1234",
+                           {"Ten-Select-Two": ["自動控制", "電力電子"]}, 4)
+        students = [student1, student2, student3]
 
+        course1 = Course({
+            "id": "Ten-Select-Two",
+            "name": "十選二實驗",
+            "type": "Ten-Select-Two",
+            "options": {
+                "電力電子": {"limit": 1, "priority": 0},
+                "自動控制": {"limit": 2, "priority": 4},
+            }
+        })
+        courses = [course1]
+
+        expected = [{
+            "studentID": "B09901186",
+            "courseName": "十選二實驗",
+            "optionName": "自動控制",
+        },
+            {
+            "studentID": "610736",
+            "courseName": "十選二實驗",
+            "optionName": "電力電子",
+        },
+            {
+            "studentID": "ABC1234",
+            "courseName": "十選二實驗",
+            "optionName": "自動控制",
+        }
+        ]
+
+        results = list_to_set(Algorithm.distribute(courses, students))
+        expected = list_to_set(expected)
+
+        self.assertEqual(results, expected)
+
+    def test_07_priority_5(self): # test for priority = 5
+        student4 = Student("Mike", "AAA5678",
+                           {"Ten-Select-Two": ["自動控制", "電力電子"]}, 1)
+        student5 = Student("Pekoli", "BBB2021",
+                           {"Ten-Select-Two": ["自動控制", "電力電子"]}, 2)
+        student1 = Student("Michael", "B09901186",
+                           {"Ten-Select-Two": ["自動控制", "電力電子"]}, 3)
+        student2 = Student("Mecoli", "610736",
+                           {"Ten-Select-Two": ["自動控制", "電力電子"]}, 4)
+        student3 = Student("Michael Jackson", "ABC1234",
+                           {"Ten-Select-Two": ["自動控制", "電力電子"]}, 4)
+        students = [student1, student2, student3, student4, student5]
+
+        course1 = Course({
+            "id": "Ten-Select-Two",
+            "name": "十選二實驗",
+            "type": "Ten-Select-Two",
+            "options": {
+                "電力電子": {"limit": 2, "priority": 0},
+                "自動控制": {"limit": 3, "priority": 5},
+            }
+        })
+        courses = [course1]
+
+        expected = [{
+            "studentID": "B09901186",
+            "courseName": "十選二實驗",
+            "optionName": "自動控制",
+        },
+            {
+            "studentID": "610736",
+            "courseName": "十選二實驗",
+            "optionName": "自動控制",
+        },
+            {
+            "studentID": "ABC1234",
+            "courseName": "十選二實驗",
+            "optionName": "自動控制",
+        },
+            {
+            "studentID": "AAA5678",
+            "courseName": "十選二實驗",
+            "optionName": "電力電子",
+        },
+            {
+            "studentID": "BBB2021",
+            "courseName": "十選二實驗",
+            "optionName": "電力電子",
+        }
+        ]
+
+        results = list_to_set(Algorithm.distribute(courses, students))
+        expected = list_to_set(expected)
+
+        self.assertEqual(results, expected)
+
+    def test_08_ten_select_two(self):  # test for bigger number (100) of students
         students = []
         for i in range(100):
             students.append(Student("Michael", "B09901186"+str(i),
@@ -145,8 +269,7 @@ class Test(unittest.TestCase):
 
         self.assertEqual(len(results), 50)
 
-    def test_06_priority_4_3(self):  # 測preselect
-
+    def test_09_preselect(self):  # test for preselect
         student1 = Student("Michael", "B09901186",
                            {"course1": ["電力電子", "自動控制", "數電實驗"]}, 3)
         student2 = Student("Mecoli", "610736",
@@ -195,6 +318,161 @@ class Test(unittest.TestCase):
         expected = list_to_set(expected)
         self.assertEqual(results, expected)
 
+    def test_10(self): # test for normal situation
+        student1 = Student("Michael", "B09901186",
+                           {"course1": ["電力電子", "自動控制", "嵌入式系統"]}, 1)
+        student2 = Student("Mecoli", "610736",
+                           {"course1": ["自動控制", "嵌入式系統", "半導體"]}, 3)
+        students = [student1, student2]
+
+        course1 = Course({
+            "id": "course1",
+            "name": "Ten-Select-Two",
+            "type": "Ten-Select-Two",
+            "description": "",
+            "options": {
+                "電力電子": {"limit": 1, "priority": 0},
+                "自動控制": {"limit": 1, "priority": 0},
+                "半導體": {"limit": 1, "priority": 0},
+                "嵌入式系統": {"limit": 1, "priority": 0}
+            }
+        })
+        courses = [course1]
+
+        expected = [
+            {
+                "studentID": "B09901186",
+                "courseName": "Ten-Select-Two",
+                "optionName": "電力電子",
+            },
+            {
+                "studentID": "610736",
+                "courseName": "Ten-Select-Two",
+                "optionName": "嵌入式系統",
+            },
+            {
+                "studentID": "610736",
+                "courseName": "Ten-Select-Two",
+                "optionName": "自動控制",
+            },
+        ]
+        results = list_to_set(Algorithm.distribute(courses, students))
+        expected = list_to_set(expected)
+        self.assertEqual(results, expected)
+
+    def test_11(self): # test for more courses than students
+        student1 = Student("Michael", "B09901186",
+                           {"course1": ["電力電子", "自動控制", "嵌入式系統"]}, 3)
+        student2 = Student("Mecoli", "610736",
+                           {"course1": ["自動控制", "嵌入式系統", "半導體"]}, 1)
+        students = [student1, student2]
+
+        course1 = Course({
+            "id": "course1",
+            "name": "Ten-Select-Two",
+            "type": "Ten-Select-Two",
+            "description": "",
+            "options": {
+                "電力電子": {"limit": 2, "priority": 0},
+                "自動控制": {"limit": 2, "priority": 0},
+                "半導體": {"limit": 2, "priority": 0},
+                "嵌入式系統": {"limit": 2, "priority": 0}
+            }
+        })
+        courses = [course1]
+
+        expected = [
+            {
+                "studentID": "B09901186",
+                "courseName": "Ten-Select-Two",
+                "optionName": "電力電子",
+            },
+            {
+                "studentID": "B09901186",
+                "courseName": "Ten-Select-Two",
+                "optionName": "自動控制",
+            },
+            {
+                "studentID": "610736",
+                "courseName": "Ten-Select-Two",
+                "optionName": "嵌入式系統",
+            },
+            {
+                "studentID": "610736",
+                "courseName": "Ten-Select-Two",
+                "optionName": "自動控制",
+            },
+        ]
+        results = list_to_set(Algorithm.distribute(courses, students))
+        expected = list_to_set(expected)
+        self.assertEqual(results, expected)
+
+    def test_12_EElab_1(self): # test for EE-Lab
+        student1 = Student("Michael", "B09901186",
+                           {"Electronic-Circuits-Experiment": ["teacher1a", "teacher1b", "teacher1c"]}, 2)
+        student2 = Student("Mecoli", "610736",
+                           {"Electronic-Circuits-Experiment": ["teacher1a", "teacher1c", "teacher1b"]}, 3)
+        student3 = Student("Pekoli", "VIP8888",
+                           {"Electronic-Circuits-Experiment": ["teacher1c"]}, 4)
+        students = [student1, student2, student3]
+
+        course1 = Course({
+            "id": "Electronic-Circuits-Experiment",
+            "name": "電路學實驗",
+            "type": "EE-Lab",
+            "description": "",
+            "options": {
+                "teacher1a": {"limit": 1, "priority": 0},
+                "teacher1b": {"limit": 1, "priority": 0},
+                "teacher1c": {"limit": 1, "priority": 0}
+            }
+        })
+        courses = [course1]
+
+        results = list_to_set(Algorithm.distribute(courses, students))
+        self.assertEqual(len(results), 3)
+
+    def test_13_EElab_2(self): # test for EE-Lab
+        student1 = Student("Michael", "B09901186",
+                           {"Electronic-Circuits-Experiment": ["teacher1a", "teacher1b", "teacher1c"]}, 2)
+        student2 = Student("Mecoli", "610736",
+                           {"Electronic-Circuits-Experiment": ["teacher1a", "teacher1c", "teacher1b"]}, 3)
+        student3 = Student("Pekoli", "VIP888",
+                           {"Electronic-Circuits-Experiment": ["teacher1c", "teacher1b", "teacher1a"]}, 4)
+        students = [student1, student2, student3]
+
+        course1 = Course({
+            "id": "Electronic-Circuits-Experiment",
+            "name": "電路學實驗",
+            "type": "EE-Lab",
+            "description": "",
+            "options": {
+                "teacher1a": {"limit": 2, "priority": 0},
+                "teacher1b": {"limit": 2, "priority": 0},
+                "teacher1c": {"limit": 2, "priority": 0}
+            }
+        })
+        courses = [course1]
+        expected = [
+            {
+                "studentID": "B09901186",
+                "courseName": "電路學實驗",
+                "optionName": "teacher1a",
+            },
+            {
+                "studentID": "610736",
+                "courseName": "電路學實驗",
+                "optionName": "teacher1a",
+            },
+            {
+                "studentID": "VIP888",
+                "courseName": "電路學實驗",
+                "optionName": "teacher1c",
+            },
+        ]
+        results = list_to_set(Algorithm.distribute(courses, students))
+        expected = list_to_set(expected)
+        self.assertEqual(results, expected)
 
 if __name__ == "__main__":
     unittest.main()
